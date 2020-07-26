85d9839e2b72492ae4131c2780c015ae319a9c80
Telephone <telephone.tan@qq.com>
2020-07-26 11:23:51 +0800

    add Gson/Hour class

---
+++ app/src/main/java/com/telephone/coursetable/Gson/Hour.java
@@ -0,0 +1,29 @@
+package com.telephone.coursetable.Gson;
+
+public class Hour {
+    private String term;
+    private String nodeno;
+    private long xss;
+    private String nodename;
+    private String memo;
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getNodeno() {
+        return nodeno;
+    }
+
+    public long getXss() {
+        return xss;
+    }
+
+    public String getNodename() {
+        return nodename;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+}
