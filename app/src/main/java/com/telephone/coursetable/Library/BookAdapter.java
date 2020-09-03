2bc916debda9f8b053e59cb4d8a2e6814af9cd16
sjj <1499127155@qq.com>
2020-09-03 22:15:21 +0800

    library search

---
+++ app/src/main/java/com/telephone/coursetable/Library/BookAdapter.java
@@ -0,0 +1,217 @@
+package com.telephone.coursetable.Library;
+
+import android.content.Context;
+import android.database.DataSetObservable;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import com.telephone.coursetable.R;
+
+import java.util.List;
+import java.util.Map;
+
+public class BookAdapter implements ExpandableListAdapter {
+
+    private Context context;
+
+    private DataSetObservable dataSetObservable;
+    private Map.Entry<List<Map.Entry<String, String>>, List<List<Map.Entry<String, String>>>> groups;
+    private boolean singleExpanded;
+    private ExpandableListView list;
+
+    public BookAdapter(Context context, Map.Entry<List<Map.Entry<String, String>>, List<List<Map.Entry<String, String>>>> groups, boolean singleExpanded, ExpandableListView list) {
+        this.context = context;
+        this.dataSetObservable = new DataSetObservable();
+        this.groups = groups;
+        this.singleExpanded = singleExpanded;
+        this.list = list;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+        dataSetObservable.registerObserver(dataSetObserver);
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+        dataSetObservable.unregisterObserver(dataSetObserver);
+    }
+
+    @Override
+    public int getGroupCount() {
+        if (groups == null) return 0;
+        return 1;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return groups.getValue().size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return groups;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return groups.getValue().get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean isExpanded, View view, ViewGroup viewGroup) {
+
+
+        if(view == null){
+            view = ((LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.library_infoes_activity, viewGroup, false);
+        }
+
+        if (groups.getKey().size()==14) {
+            ((TextView) view.findViewById(R.id.message_info_id)).setText("    "+groups.getKey().get(0).getKey()+groups.getKey().get(0).getValue());
+            ((TextView) view.findViewById(R.id.message_info_name)).setText("    "+groups.getKey().get(1).getKey()+groups.getKey().get(1).getValue());
+            ((TextView) view.findViewById(R.id.message_info_author)).setText("    "+groups.getKey().get(2).getKey()+groups.getKey().get(2).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publisher)).setText("    "+groups.getKey().get(3).getKey()+groups.getKey().get(3).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publishdate)).setText("    "+groups.getKey().get(4).getKey()+groups.getKey().get(4).getValue());
+            ((TextView) view.findViewById(R.id.message_info_isbn)).setText("    "+groups.getKey().get(5).getKey()+groups.getKey().get(5).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ssh)).setText("    "+groups.getKey().get(6).getKey()+groups.getKey().get(6).getValue());
+            ((TextView) view.findViewById(R.id.message_info_flh)).setText("    "+groups.getKey().get(7).getKey()+groups.getKey().get(7).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ys)).setText("    "+groups.getKey().get(8).getKey()+groups.getKey().get(8).getValue());
+            ((TextView) view.findViewById(R.id.message_info_jg)).setText("    "+groups.getKey().get(9).getKey()+groups.getKey().get(9).getValue());
+            ((TextView) view.findViewById(R.id.message_info_fbs)).setText("    "+groups.getKey().get(10).getKey()+groups.getKey().get(10).getValue());
+            ((TextView) view.findViewById(R.id.message_info_zgs)).setText("    "+groups.getKey().get(11).getKey()+groups.getKey().get(11).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljts)).setText("    "+groups.getKey().get(12).getKey()+groups.getKey().get(12).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljcs)).setText("    "+groups.getKey().get(13).getKey()+groups.getKey().get(13).getValue());
+        }
+
+        if (groups.getKey().size()==13) {
+            ((TextView) view.findViewById(R.id.message_info_id)).setText("    "+groups.getKey().get(0).getKey()+groups.getKey().get(0).getValue());
+            ((TextView) view.findViewById(R.id.message_info_name)).setText("    "+groups.getKey().get(1).getKey()+groups.getKey().get(1).getValue());
+            ((TextView) view.findViewById(R.id.message_info_author)).setText("    "+groups.getKey().get(2).getKey()+groups.getKey().get(2).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publisher)).setText("    "+groups.getKey().get(3).getKey()+groups.getKey().get(3).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publishdate)).setText("    "+groups.getKey().get(4).getKey()+groups.getKey().get(4).getValue());
+            ((TextView) view.findViewById(R.id.message_info_isbn)).setText("    "+groups.getKey().get(5).getKey()+groups.getKey().get(5).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ssh)).setText("    "+groups.getKey().get(6).getKey()+groups.getKey().get(6).getValue());
+            ((TextView) view.findViewById(R.id.message_info_flh)).setText("    "+"分类号：");
+            ((TextView) view.findViewById(R.id.message_info_ys)).setText("    "+groups.getKey().get(7).getKey()+groups.getKey().get(7).getValue());
+            ((TextView) view.findViewById(R.id.message_info_jg)).setText("    "+groups.getKey().get(8).getKey()+groups.getKey().get(8).getValue());
+            ((TextView) view.findViewById(R.id.message_info_fbs)).setText("    "+groups.getKey().get(9).getKey()+groups.getKey().get(9).getValue());
+            ((TextView) view.findViewById(R.id.message_info_zgs)).setText("    "+groups.getKey().get(10).getKey()+groups.getKey().get(10).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljts)).setText("    "+groups.getKey().get(11).getKey()+groups.getKey().get(11).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljcs)).setText("    "+groups.getKey().get(12).getKey()+groups.getKey().get(12).getValue());
+        }
+
+        if (groups.getKey().size()==12) {
+            ((TextView) view.findViewById(R.id.message_info_id)).setText("    "+groups.getKey().get(0).getKey()+groups.getKey().get(0).getValue());
+            ((TextView) view.findViewById(R.id.message_info_name)).setText("    "+groups.getKey().get(1).getKey()+groups.getKey().get(1).getValue());
+            ((TextView) view.findViewById(R.id.message_info_author)).setText("    "+groups.getKey().get(2).getKey()+groups.getKey().get(2).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publisher)).setText("    "+groups.getKey().get(3).getKey()+groups.getKey().get(3).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publishdate)).setText("    "+groups.getKey().get(4).getKey()+groups.getKey().get(4).getValue());
+            ((TextView) view.findViewById(R.id.message_info_isbn)).setText("    "+groups.getKey().get(5).getKey()+groups.getKey().get(5).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ssh)).setText("    "+groups.getKey().get(6).getKey()+groups.getKey().get(6).getValue());
+            ((TextView) view.findViewById(R.id.message_info_flh)).setText("    "+"分类号：");
+            ((TextView) view.findViewById(R.id.message_info_ys)).setText("    "+groups.getKey().get(7).getKey()+groups.getKey().get(7).getValue());
+            ((TextView) view.findViewById(R.id.message_info_jg)).setText("    "+"价格：");
+            ((TextView) view.findViewById(R.id.message_info_fbs)).setText("    "+groups.getKey().get(8).getKey()+groups.getKey().get(8).getValue());
+            ((TextView) view.findViewById(R.id.message_info_zgs)).setText("    "+groups.getKey().get(9).getKey()+groups.getKey().get(9).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljts)).setText("    "+groups.getKey().get(10).getKey()+groups.getKey().get(10).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljcs)).setText("    "+groups.getKey().get(11).getKey()+groups.getKey().get(11).getValue());
+        }
+
+        if (groups.getKey().size()==11) {
+            ((TextView) view.findViewById(R.id.message_info_id)).setText("    "+groups.getKey().get(0).getKey()+groups.getKey().get(0).getValue());
+            ((TextView) view.findViewById(R.id.message_info_name)).setText("    "+groups.getKey().get(1).getKey()+groups.getKey().get(1).getValue());
+            ((TextView) view.findViewById(R.id.message_info_author)).setText("    "+groups.getKey().get(2).getKey()+groups.getKey().get(2).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publisher)).setText("    "+groups.getKey().get(3).getKey()+groups.getKey().get(3).getValue());
+            ((TextView) view.findViewById(R.id.message_info_publishdate)).setText("    "+groups.getKey().get(4).getKey()+groups.getKey().get(4).getValue());
+            ((TextView) view.findViewById(R.id.message_info_isbn)).setText("    "+groups.getKey().get(5).getKey()+groups.getKey().get(5).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ssh)).setText("    "+groups.getKey().get(6).getKey()+groups.getKey().get(6).getValue());
+            ((TextView) view.findViewById(R.id.message_info_flh)).setText("    "+"分类号：");
+            ((TextView) view.findViewById(R.id.message_info_ys)).setText("    "+"页数：");
+            ((TextView) view.findViewById(R.id.message_info_jg)).setText("    "+"价格：");
+            ((TextView) view.findViewById(R.id.message_info_fbs)).setText("    "+groups.getKey().get(7).getKey()+groups.getKey().get(7).getValue());
+            ((TextView) view.findViewById(R.id.message_info_zgs)).setText("    "+groups.getKey().get(8).getKey()+groups.getKey().get(8).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljts)).setText("    "+groups.getKey().get(9).getKey()+groups.getKey().get(9).getValue());
+            ((TextView) view.findViewById(R.id.message_info_ljcs)).setText("    "+groups.getKey().get(10).getKey()+groups.getKey().get(10).getValue());
+        }
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean isLastChild, View view, ViewGroup viewGroup) {
+
+        if(view == null){
+            view = ((LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.library_locals_activity, viewGroup, false);
+        }
+
+        //i是有几个列表，i1是键值对中值列表
+        ((TextView)view.findViewById(R.id.message_local_bookid)).setText("          "+groups.getValue().get(i1).get(0).getKey()+groups.getValue().get(i1).get(0).getValue());
+        ((TextView)view.findViewById(R.id.message_local_barcode)).setText("          "+groups.getValue().get(i1).get(1).getKey()+groups.getValue().get(i1).get(1).getValue());
+        ((TextView)view.findViewById(R.id.message_local_callno)).setText("          "+groups.getValue().get(i1).get(2).getKey()+groups.getValue().get(i1).get(2).getValue());
+        ((TextView)view.findViewById(R.id.message_local_localstatu)).setText("          "+groups.getValue().get(i1).get(3).getKey()+groups.getValue().get(i1).get(3).getValue());
+        ((TextView)view.findViewById(R.id.message_local_local)).setText("          "+groups.getValue().get(i1).get(4).getKey()+groups.getValue().get(i1).get(4).getValue());
+        ((TextView)view.findViewById(R.id.message_local_cirType)).setText("          "+groups.getValue().get(i1).get(5).getKey()+groups.getValue().get(i1).get(5).getValue());
+
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return groups.getKey().isEmpty();
+    }
+
+    @Override
+    public void onGroupExpanded(int gp) {
+        if (singleExpanded) {
+            for (int i = 0; i < groups.getKey().size(); i++){
+                if (gp != i){
+                    list.collapseGroup(i);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onGroupCollapsed(int i) {
+
+    }
+
+    @Override
+    public long getCombinedChildId(long gp, long cp) {
+        return gp * 1000000 + (cp + 1);
+    }
+
+    @Override
+    public long getCombinedGroupId(long gp) {
+        return gp * 1000000;
+    }
+}
---
+++ app/src/main/java/com/telephone/coursetable/Library/Show.java
@@ -0,0 +1,102 @@
+package com.telephone.coursetable.Library;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Show {
+
+    public static  List<List<Map.Entry<String, String>>>  getBookInfo(String html){
+        List<List<Map.Entry<String, String>>> books = new LinkedList<>();
+        List<Map.Entry<String, String>> bookinfoes;
+        Document doc = Jsoup.parse(html);
+        Elements book_infoes = doc.select("html > body > form#form1 > div.body > div.mainbody2_out > div.mainbody2_in > div.mainbody > div.content > div.main > ul.resultlist ");
+        for (int i = 0; i < book_infoes.size(); i++){
+
+            Element book = book_infoes.get(i);
+
+            bookinfoes = new LinkedList<>();
+
+            Elements bokkids = book.select("ul.resultlist > li > input#StrTmpRecno.inputISBN");
+            String bokkid = bokkids.attr("value");
+            bookinfoes.add(Map.entry("书籍编号：", bokkid));
+//            Log.e("书籍编号：", bokkid);
+
+            Elements titles = book.select("ul.resultlist > li > div.into > h3.title > a");
+            String title = titles.get(0).ownText();
+            bookinfoes.add(Map.entry("名称：", title));
+//            Log.e("名称：", title);
+
+            Element titlebar = book.select("ul.resultlist > li > div.into > div.titbar").get(0);
+            Elements spans = titlebar.select("span");
+            for (Element span : spans){
+                String name = span.ownText();
+                String value = span.select("span > strong").get(0).ownText();
+                if( value.equals("v., :") ) value="";
+                bookinfoes.add(Map.entry(name, value));
+//                Log.e(name, value);
+            }
+
+            books.add(bookinfoes);
+//            Log.e("#######", "###############################");
+        }
+        return books;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    public static List<List<List<Map.Entry<String, String>>>> getBookLocal(String xml) {
+        Document doc_xml = Jsoup.parse(xml);
+        Elements statuses = doc_xml.select("bookinfo > books");
+        List<List<List<Map.Entry<String, String>>>> books = new LinkedList<>();
+        List<List<Map.Entry<String, String>>> locals;
+        for(Element local : statuses) {
+            locals = new LinkedList<>();
+            Elements bs = local.select("books > book");
+            for (Element b : bs){
+                List<Map.Entry<String, String>> local_node = new LinkedList<>();
+
+                local_node.add(Map.entry("图书编号：", b.select("book > bookid ").get(0).ownText()));
+                local_node.add(Map.entry("条码号：", b.select("book > barcode ").get(0).ownText()));
+                local_node.add(Map.entry("索书号：", b.select("book > callno ").get(0).ownText()));
+                local_node.add(Map.entry("馆藏状态：", b.select("book > localstatu ").get(0).ownText()));
+                local_node.add(Map.entry("馆藏地点：", b.select("book > local ").get(0).ownText()));
+                local_node.add(Map.entry("流通类型：", b.select("book > cirType ").get(0).ownText()));
+                local_node.add(Map.entry("预约处理：", b.select("book > servicePoint ").get(0).ownText()));
+                local_node.add(Map.entry("出借数据编号：", b.select("book > loanDatanum ").get(0).ownText()));
+                local_node.add(Map.entry("登录号：", b.select("book > loannum ").get(0).ownText()));
+                local_node.add(Map.entry("藏书编号：", b.select("book > hldcount ").get(0).ownText()));
+                local_node.add(Map.entry("所有藏书编号：", b.select("book > hldallnum ").get(0).ownText()));
+
+//                Log.e("图书编号", b.select("book > bookid ").get(0).ownText());
+//                Log.e("条码号",  b.select("book > barcode ").get(0).ownText());
+//                Log.e("索书号",  b.select("book > callno ").get(0).ownText());
+//                Log.e("馆藏状态", b.select("book > localstatu ").get(0).ownText());
+//                Log.e("馆藏地点", b.select("book > local ").get(0).ownText());
+//                Log.e("流通类型", b.select("book > cirType ").get(0).ownText());
+//                Log.e("预约处理", b.select("book > servicePoint ").get(0).ownText());
+//                Log.e("出借数据编号", b.select("book > loanDatanum ").get(0).ownText());
+//                Log.e("登录号", b.select("book > loannum ").get(0).ownText());
+//                Log.e("藏书编号", b.select("book > hldcount ").get(0).ownText());
+//                Log.e("所有藏书编号", b.select("book > hldallnum ").get(0).ownText());
+
+                locals.add(local_node);
+
+                Log.e("...................", ".............................");
+            }
+            books.add(locals);
+        }
+        return books;
+    }
+
+
+}
